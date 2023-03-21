2bc9e237b1e88e6061c71a4bee3eef1e9c42e7f1
felipe2640 <felipe_brito09@hotmail.com>
2023-03-21 19:50:29 -0300

    feat(Include multer): Create server
    Create server to upload images.

--- fakeAPI/server.js
+++ fakeAPI/server.js
@@ -1,20 +1,41 @@
-
-const bodyParser = require("body-parser");
-const jsonServer = require("json-server");
-
-
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-
-const router = jsonServer.router("./db.json");
-
-server.use(jsonServer.defaults());
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+const multer = require('multer');
 
 
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads'); // pasta onde serão armazenadas as imagens
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // nome original do arquivo
+  },
+
+});
+
+const upload = multer({ storage: storage });
+
+server.post('/posts', upload.single('image'), (req, res) => {
+  const file = req.file;
+  const url = `http://localhost:8080/uploads/${file.originalname}`;
+  const image = {
+    id: Date.now(),
+    img: url,
+    name: req.body.name,
+    message: req.body.message,
+
+  };
+  router.db.get('images').push(image).write();
+  res.json(image);
+});
 
 server.use(router);
-
 server.listen(process.env.PORT ? process.env.PORT : 8080, () => {
-  console.log(`Servidor inicializado`);
+  console.log('JSON Server is running');
 });
